47ffa9444159cef46a266d239b8c4c74925e1168
leonardobenedeti <leonardobenedeti@gmail.com>
2018-04-17 17:50:03 -0300

    Inclusao da lib themeable browser e customização do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { ThemeableBrowser } from '@ionic-native/themeable-browser';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    ThemeableBrowser,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { ThemeableBrowser, ThemeableBrowserOptions, ThemeableBrowserObject } from '@ionic-native/themeable-browser';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, private themeableBrowser: ThemeableBrowser) {}
 
-  constructor(public navCtrl: NavController) {
+  openBrowser(){
+    const options: ThemeableBrowserOptions = {
+      statusbar: {
+          color: '#ee3224'
+      },
+      toolbar: {
+          height: 44,
+          color: '#ee3224'
+      },
+      title: {
+          color: '#ffffff',
+          staticText: 'Chat'
+      },
+      closeButton: {
+          wwwImage: 'assets/imgs/close.png',
+          align: 'right',
+          event: 'closePressed'
+      },
+      backButtonCanClose: true
+  };
 
+
+    const browser: ThemeableBrowserObject = this.themeableBrowser.create('https://ionic.io', '_blank', options);
+    browser.on('closePressed').subscribe(data => {
+      browser.close();
+    });
   }
-
 }
